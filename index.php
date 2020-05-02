bcdfecf067daab2d702bc2039cfda923b4ae40a6
Nemolite <g25092011@mail.ru>
2020-05-02 22:35:35 +0300

    remphp1.0.1

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // This is repetition
- foreach ([1,2,3,4] as $mNum){
+ foreach ([1,2,'3',4] as $mNum){
 	 $isItNum = gettype($mNum);
 	 print "Is the $isItNum = $mNum";
 	 if (is_numeric($mNum)){
@@ -10,5 +10,12 @@
 	 }
 		 echo "<br />";
  }
-	 
+	// no2
+
+$delta = 0.00001;
+$a = 1.00000001;
+$b = 1.00000000	;
+if (abs($a-$b)<$delta) {
+	print "$a and $b are equal enough";
+}
 ?>
